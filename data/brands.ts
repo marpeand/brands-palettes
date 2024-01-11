f1c1770cf9ab203a04193da1012a65cc161e1f4d
marpeand <55035865+marpeand@users.noreply.github.com>
2024-01-11 13:32:57 -0300

    add brands palettes

--- data/brands.ts
+++ data/brands.ts
@@ -1,3 +1,109 @@
 import { Brand } from '@/types/brand';
 
-export const brands: Brand = {};
+export const brands: Brand = {
+  ai: {
+    codium: ['#5866EE', '#0ADAB1', '#351DB6', '#148C8C'],
+    bard: ['#A971B4', '#3894DD', '#6485D4', '#E17465'],
+    openai: ['#10A37F'],
+    huggingface: ['#F1C445', '#0B101D', '#FC343C']
+  },
+  browser: {
+    firefox: ['#F65D40', '#FAE17D', '#8A49DC', '#703599'],
+    brave: ['#FC3A07', '#FCD0C3', '#FC8054', '#FC9F6F'],
+    chrome: ['#239A45', '#1574EB', '#FBC211', '#E13628'],
+    opera: ['#F11428', '#B4070D', '#F74545'],
+    chromium: ['#B4CCFB', '#649CF4', '#146ADD'],
+    tor: ['#6C1497', '#F0E0F9', '#941DD4', '#B572DC'],
+    bing: ['#40C4EC', '#1B5CF1', '#2180F4']
+  },
+  cryptocurrencies: {
+    bitcoin: ['#F79621', '#FFFFFF'],
+    dogecoin: ['#C4A851', '#F1E7B9', '#363217', '#6C5C2C'],
+    monero: ['#FC6404', '#4D4D4D'],
+    tether: ['#50AF95'],
+    ethereum: ['#637BEC', '#DADEF2', '#8494EC', '#A4B4F4'],
+    bnb: ['#F4BB0B'],
+    solana: ['#BD3EF2', '#26D8B3', '#708DD2', '#AC54EC'],
+    cardano: ['#0434AB'],
+    litecoin: ['#345C9C'],
+    uniswap: ['#FF007B']
+  },
+  crypto: {
+    opensea: ['#2081e2'],
+    metaMask: ['#773b0e', '#f68614', '#1e3145'],
+    coinmarketcap: ['#2b6aff'],
+    coinbase: ['#0051ff'],
+    trustwallet: ['#3375bb'],
+    binance: ['#333333', '#f0b90b'],
+    kraken: ['#5841d9']
+  },
+  companies: {
+    meta: ['#0576EF', '##79C4F9', '#5EA0E3'],
+    microsoft: ['#04ACF4', '#82BD01', '#F45424', '#FFBB02'],
+    intel: ['#0474C4'],
+    tesla: ['#E61134'],
+    nvidia: ['#75B800']
+  },
+  databases: {
+    supabase: ['#3AC889', '#248F5F', '#2CA46C'],
+    mongoDB: ['#07A04C', '#1C2C3C', '#BBC3C3', '#09934E'],
+    postgreSQL: ['#2C6494', '#040404'],
+    redis: ['#c6302b', '#912626', '#9a2928', '#621b1c']
+  },
+  education: {
+    udemy: ['#a632f0'],
+    platzi: ['#9BCB3C', '#4C4C4C'],
+    coursera: ['#0055d3', '#ffffff'],
+    khanAcademy: ['#0bc097', '#032566'],
+    udacity: ['#00b5e5', '#2114ff']
+  },
+  entertainment: {
+    netflix: ['#C20710', '#040404', '#630B0D', '#310406'],
+    starplus: ['#241B3A', '#F64528'],
+    hulu: ['#15e884'],
+    primevideo: ['#04ACE4', '#1C2C3C', '#2CB8E8'],
+    disneyplus: ['#0B167D', '#1930F6', '#69A7FC']
+  },
+  language: {
+    python: ['#3774a7', '#ffd647'],
+    java: ['#e76c01', '#5382a1', '#ca3132'],
+    cpp: ['#649ad2', '#004482', '#00589c'],
+    javascript: ['#f0db4f', '#323330'],
+    html: ['#e44c24', '#f16529'],
+    css: ['#214ce5', '#2465f1'],
+    typescript: ['#2d79c7'],
+    swift: ['#fd2f22', '#f97432'],
+    sass: ['#ce679a']
+  },
+  platforms: {
+    heroku: ['#410099'],
+    github: ['#171515'],
+    digitalOcean: ['#0081ff'],
+    firebase: ['#FBCA2A', '#F99209'],
+    aws: ['#232f3e', '#ff9900']
+  },
+  software: {
+    bitwarden: ['#105ddd'],
+    vscode: ['#0878ba', '#3c99d2', '#2389cb'],
+    discord: ['#5865f2'],
+    docker: ['#1d63ed'],
+    git: ['#f05033'],
+    gmail: ['#4086f4', '#c61d19', '#eb4132', '#fbbd00', '#31aa52'],
+    npm: ['#cc3534'],
+    postman: ['#ff6c37'],
+    sublimetext: ['#454545', '#ff9800'],
+    twilio: ['#ee3248']
+  },
+  social: {
+    twitter: ['#179CF0'],
+    x: ['#FFFFFF', '#000000'],
+    whatsapp: ['#40C351'],
+    reddit: ['#FC4404', '#FCD6C9', '#FC8361', '#FC9C7C'],
+    pinterest: ['#BC0414'],
+    linkedin: ['#0170AD'],
+    youtube: ['#FF0000'],
+    telegram: ['#0088CC'],
+    mastodon: ['#5D4FE8', '#DAD8FA', '#ACA4F8', '#928AF4'],
+    facebook: ['#0C64FC', '#C2DAFC', '#619DFC']
+  }
+};
